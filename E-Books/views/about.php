31bb9a2702d1f26341d0859015f5d85d1228460e
Umer Abdul Wahid <umerwahiid48@gmail.com>
2022-07-22 17:27:13 +0500

    umer/5:27/22

--- E-Books/views/about.php
+++ E-Books/views/about.php
@@ -1,17 +1,32 @@
+<?php
+
+    include "../apis/connection.php";
+    session_start();
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!-- font awesome cdn link  -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
-        <!-- custom css file link  -->
-        <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
 
-        <!-- short icon link -->
-        <link rel="shortcut icon" type="image/png" href="image/loader-img.gif">
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- short icon link -->
+    <link rel="shortcut icon" type="image/png" href="image/loader-img.gif">
+
+    <!-- bootstrap 5 css link -->
+    <link rel="stylesheet" href="css/bootstrap-5.0.2-dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 
     <title>About</title>
 </head>
@@ -60,6 +75,8 @@
 <img src="image/loader-img.gif" alt="">
 </div> -->
 
-
+    <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+    <!-- custom js file link  -->
+    <script src="js/script.js"></script>
 </body>
 </html>
--- E-Books/views/contact.php
+++ E-Books/views/contact.php
@@ -1,19 +1,32 @@
+<?php
+
+    include "../apis/connection.php";
+    session_start();
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
 
     <!-- font awesome cdn link  -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
 
     <!-- custom css file link  -->
     <link rel="stylesheet" href="css/style.css">
 
     <!-- short icon link -->
     <link rel="shortcut icon" type="image/png" href="image/loader-img.gif">
+
+    <!-- bootstrap 5 css link -->
+    <link rel="stylesheet" href="css/bootstrap-5.0.2-dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <title>Contact</title>
 </head>
 <body>
@@ -133,9 +146,7 @@
 
 
 
-
     <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
-
     <!-- custom js file link  -->
     <script src="js/script.js"></script>
 
