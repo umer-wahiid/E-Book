cae6477c8b4169a43bca5d1b127102afb5d5b9c6
ansari7016 <ammaransari7016@gmail.com>
2022-08-05 19:04:19 +0500

    ammar/7:04/05

--- E-Books/views/index.php
+++ E-Books/views/index.php
@@ -1,7 +1,7 @@
     <?php
 
-    require '../apis/connection.php';
     session_start();
+    require '../apis/connection.php';
     // if(isset($_GET['alert'])){
     //     $alert = $_GET['alert'];
     //     echo '<div class="alert alert-success" role="alert">'.$alert.'</div>';
