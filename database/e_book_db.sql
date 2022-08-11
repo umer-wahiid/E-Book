acf1070b72f13a81b2b9a80301806407489a578d
ansari7016 <ammaransari7016@gmail.com>
2022-08-11 13:43:41 +0500

    ammar/1:43/11

--- database/e_book_db.sql
+++ database/e_book_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 05, 2022 at 04:02 PM
+-- Generation Time: Aug 11, 2022 at 10:42 AM
 -- Server version: 10.4.8-MariaDB
 -- PHP Version: 7.3.11
 
@@ -25,6 +25,21 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_book_cart`
+--
+
+CREATE TABLE `tbl_book_cart` (
+  `cart_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `cart_name` varchar(25) NOT NULL,
+  `cart_price` varchar(25) NOT NULL,
+  `cart_quantity` varchar(50) NOT NULL,
+  `cart_image` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_book_category`
 --
 
@@ -42,7 +57,8 @@ INSERT INTO `tbl_book_category` (`b_id`, `b_category`) VALUES
 (7, 'Novels'),
 (8, 'Story Books'),
 (9, 'Quiz Books'),
-(10, 'Journals');
+(10, 'Journals'),
+(11, 'Best Sellers');
 
 -- --------------------------------------------------------
 
@@ -76,11 +92,31 @@ CREATE TABLE `tbl_book_detail` (
 --
 
 INSERT INTO `tbl_book_detail` (`book_id`, `book_title`, `book_author`, `book_author_about`, `book_publisher`, `book_publication_date`, `book_weight`, `book_pages`, `book_rating`, `book_availability`, `book_about`, `book_category_1`, `book_category_2`, `book_category_3`, `book_price_hardcopy`, `book_price_pdf`, `book_price_cd`, `book_image`) VALUES
-(6, 'A Gentleman In Moscow', 'Amor Towels', 'Amor Towles is the author of the New York Times bestsellers Rules of Civility and A Gentleman in Moscow. The two novels have collectively sold more than four million copies and have been translated into more than thirty languages. Towles lives in Manhattan with his wife and two children.', 'Windmill Books', '2017-11-02', 100, 480, 5, 'PDF/CD/Book', 'n 21 June 1922, Count Alexander Rostov - recipient of the Order of Saint Andrew, member of the Jockey Club, Master of the Hunt - is escorted out of the Kremlin, across Red Square and through the elegant revolving doors of the Hotel Metropol.\r\n\r\nDeemed an unrepentant aristocrat by a Bolshevik tribunal, the Count has been sentenced to house arrest indefinitely. But instead of his usual suite, he must now live in an attic room while Russia undergoes decades of tumultuous upheaval.', 6, 7, 8, 2095, 1100, 1400, '../apis/book_image/62e936ab48b36A Gentleman in Moscow.jpg'),
-(7, 'Love', 'Amor Zusak', 'dwad', 'Windmill Books', '2018-08-09', 144, 654, 2, 'PDF/CD/Book', 'daw', 9, 10, 7, 23, 432, 432, '../apis/book_image/62ea78d6ece51book-9.png'),
-(8, 'Thanks', 'Zuskak Towels', 'kwtq', 'Windmill Books', '2002-08-09', 871, 534, 5, 'PDF/CD/Book', 'age', 8, 9, 10, 300, 180, 225, '../apis/book_image/62ea79661356abook5.png'),
-(9, 'The Art City', 'Markus Towels', 'kudywq', 'Windmill Books', '1988-08-06', 766, 869, 4, 'PDF/CD/Book', 'jdfs', 7, 6, 10, 300, 180, 225, '../apis/book_image/62ea7c2e88213book-1.png'),
-(10, 'Story Of Liberty', 'Amor Markus', 'afaf', 'E-Books', '1987-05-07', 767, 865, 5, 'PDF/CD/Book', 'da', 8, 9, 10, 300, 180, 225, '../apis/book_image/62ea7c899484cbook-4.png');
+(15, 'The Book Thief', 'Markus Zusak', 'awdada', 'Windmill Books', '2017-09-08', 456, 624, 5, 'PDF/CD/Book', 'afa', 6, 11, 7, 1700, 1000, 1300, '../apis/book_image/62f37d5930692The Book Thief.png'),
+(16, 'Love', 'Zuskak Towels', 'vseat', 'E-Books', '2006-09-07', 877, 567, 5, 'PDF/CD/Book', 'bfg', 11, 7, 8, 300, 180, 225, '../apis/book_image/62f37bfea18a2book-9.png'),
+(17, 'Retro', 'Zuskak Towels', 'awf', 'E-Books', '2018-04-08', 567, 654, 5, 'PDF/CD/Book', 'xWD', 8, 11, 7, 300, 180, 225, '../apis/book_image/62f37c4e31a2abook-10.png'),
+(18, 'A Gentleman In Moscow', 'Amor Towels', 'st', 'Windmill Books', '2016-09-08', 100, 480, 5, 'PDF/CD/Book', 'dsts', 9, 11, 10, 300, 180, 225, '../apis/book_image/62f37ca1df8e0A Gentleman in Moscow.png'),
+(19, 'Story Of Liberty', 'Amor Markus', 'vawf', 'Windmill Books', '2004-06-07', 464, 657, 3, 'PDF/CD/Book', 'afwr', 11, 9, 10, 300, 180, 225, '../apis/book_image/62f37ce4b025dbook-5.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_book_order`
+--
+
+CREATE TABLE `tbl_book_order` (
+  `order_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(25) NOT NULL,
+  `user_number` int(11) NOT NULL,
+  `user_email` varchar(25) NOT NULL,
+  `order_method` varchar(50) NOT NULL,
+  `user_address` varchar(500) NOT NULL,
+  `total_products` varchar(1000) NOT NULL,
+  `total_price` int(100) NOT NULL,
+  `placed_on` varchar(50) NOT NULL,
+  `payment_status` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -131,10 +167,15 @@ CREATE TABLE `tbl_user_data` (
 --
 
 INSERT INTO `tbl_user_data` (`user_id`, `user_name`, `user_email`, `user_address`, `user_phone`, `user_image`, `user_subscribtion`, `user_password`) VALUES
-(1, 'Ammar Ansari', 'ammaransari7016@gmail.com', 'House no D-18 Shah Faisal Town Malir Halt Karachi', 28374987, '../apis/profileImage/62da93457be0dAmmar.jpg', '3 Year', 'ammaransari5544'),
+(1, 'Ammar Ansari', 'ammaransari7016@gmail.com', 'House no D-18 Shah Faisal Town Malir Halt Karachi', 28374987, '../apis/profileImage/62da93457be0dAmmar.jpg', '2 Year', 'ammaransari5544'),
 (2, 'Ayan', 'ayan@gmail.com', 'ashfiuaufheiufha', 783561875, '../apis/profileImage/62daa20c215fbblog-1.jpg', 'Annual', '123456'),
 (3, 'Umar Abdul Wahid', 'umarwahiid48@gmail.com', 'fkhsahfuiashufha', 4563279, '../apis/profileImage/62dae42cd262dblog-4.jpg', 'Monthly', '1234'),
-(4, 'Imran Hafeez', 'i.emranhafeez@gmail.com', 'hsaguoaghuqy', 2147483647, 'image', 'No', '1234');
+(4, 'Imran Hafeez', 'i.emranhafeez@gmail.com', 'hsaguoaghuqy', 2147483647, 'image', 'No', '1234'),
+(5, 'Shapatar', 'shapatar@lallu.co', 'uhb76', 967, 'image', 'No', '1234'),
+(6, 'Maaz ', 'maaz@gmail.com', 'sfiuahfiuhfiuqh', 898979, 'fas fa-user', 'No', '1234'),
+(7, 'Ammar', 'ammar@gmail.com', 'House No. D-18 Shah Faisal Town Malir Halt Karachi Near Cafe Bajwa ', 2147483647, 'image', 'No', '00f1dfa98dcf9459d6309552a'),
+(8, 'Umar', 'umar@gmail.com', 'jilsjte', 523, 'image', 'No', '81dc9bdb52d04dc20036dbd83'),
+(9, 'Ayan', 'ayan65@gmail.com', 'sfet', 7356, 'image', 'No', 'e10adc3949ba59abbe56e057f');
 
 -- --------------------------------------------------------
 
@@ -148,6 +189,7 @@ CREATE TABLE `tbl_user_reviews` (
   `review_email` varchar(25) NOT NULL,
   `review_stars` varchar(25) NOT NULL,
   `review_description` varchar(1000) NOT NULL,
+  `review_approvel` varchar(25) NOT NULL,
   `review_image` varchar(1000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -155,20 +197,26 @@ CREATE TABLE `tbl_user_reviews` (
 -- Dumping data for table `tbl_user_reviews`
 --
 
-INSERT INTO `tbl_user_reviews` (`review_id`, `review_name`, `review_email`, `review_stars`, `review_description`, `review_image`) VALUES
-(1, 'Ammar Ansari', 'ammaransari7016@gmail.com', '5-Stars', 'Its Really Awsome', ''),
-(2, 'Ammar Ansari', 'ammaransari7016@gmail.com', '5-Stars', 'Its Really Awsome', '../apis/profileImage/62da93457be0dAmmar.jpg'),
-(3, 'Ammar Ansari', 'ammaransari7016@gmail.com', '4-Stars', 'Its Great I really Satisfied', '../apis/profileImage/62da93457be0dAmmar.jpg'),
-(4, 'Ammar Ansari', 'ammaransari7016@gmail.com', '4-Stars', 'Its Great I really Satisfied', '../apis/profileImage/62da93457be0dAmmar.jpg'),
-(5, 'Ammar Ansari', 'ammaransari7016@gmail.com', '4-Stars', 'Its Great I really Satisfied', '../apis/profileImage/62da93457be0dAmmar.jpg'),
-(6, '', '', '4-Stars', 'Its Great I really Satisfied', ''),
-(7, '', '', '4-Stars', 'Its Great I really Satisfied', ''),
-(8, '', '', '4-Stars', 'Its Great I really Satisfied', ''),
-(9, '', '', '4-Stars', 'Its Great I really Satisfied', '');
+INSERT INTO `tbl_user_reviews` (`review_id`, `review_name`, `review_email`, `review_stars`, `review_description`, `review_approvel`, `review_image`) VALUES
+(3, 'Ammar Ansari', 'ammaransari7016@gmail.com', '4-Stars', 'Its Great I really Satisfied', 'Yes', '../apis/profileImage/62da93457be0dAmmar.jpg'),
+(12, 'Ammar Ansari', 'ammaransari7016@gmail.com', '3-Stars', 'Its Sweat but Plz Increase The Website Features ', 'Yes', '../apis/profileImage/62da93457be0dAmmar.jpg'),
+(13, 'Ayan', 'ayan@gmail.com', '5-Stars', 'NICE EXPERIENCE KEEP IT UP', 'Yes', '../apis/profileImage/62daa20c215fbblog-1.jpg'),
+(14, 'Umar Abdul Wahid', 'umarwahiid48@gmail.com', '3-Stars', 'Awsome But Need Improvment', 'Yes', '../apis/profileImage/62dae42cd262dblog-4.jpg'),
+(15, 'Shapatar', 'shapatar@lallu.co', '1-Star', 'Ek Dum Tatti Maal Haii Website Bhe Tatti Maal BHe Tatti', '', 'image'),
+(20, 'Ammar Ansari', 'ammaransari7016@gmail.com', '5-Stars', 'rasha rasha', '', '../apis/profileImage/62da93457be0dAmmar.jpg'),
+(21, 'Ammar Ansari', 'ammaransari7016@gmail.com', '5-Stars', 'rasha rasha', 'Yes', '../apis/profileImage/62da93457be0dAmmar.jpg'),
+(22, 'Ammar Ansari', 'ammaransari7016@gmail.com', '5-Stars', 'Dagha', 'No', '../apis/profileImage/62da93457be0dAmmar.jpg'),
+(23, 'Ammar Ansari', 'ammaransari7016@gmail.com', '5-Stars', 'Dagha', 'No', '../apis/profileImage/62da93457be0dAmmar.jpg');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `tbl_book_cart`
+--
+ALTER TABLE `tbl_book_cart`
+  ADD PRIMARY KEY (`cart_id`);
 
 --
 -- Indexes for table `tbl_book_category`
@@ -184,6 +232,12 @@ ALTER TABLE `tbl_book_detail`
   ADD KEY `book_category_1` (`book_category_1`),
   ADD KEY `book_category_2` (`book_category_2`),
   ADD KEY `book_category_3` (`book_category_3`);
+
+--
+-- Indexes for table `tbl_book_order`
+--
+ALTER TABLE `tbl_book_order`
+  ADD PRIMARY KEY (`order_id`);
 
 --
 -- Indexes for table `tbl_user_contact`
@@ -208,16 +262,28 @@ ALTER TABLE `tbl_user_reviews`
 --
 
 --
+-- AUTO_INCREMENT for table `tbl_book_cart`
+--
+ALTER TABLE `tbl_book_cart`
+  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `tbl_book_category`
 --
 ALTER TABLE `tbl_book_category`
-  MODIFY `b_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `b_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `tbl_book_detail`
 --
 ALTER TABLE `tbl_book_detail`
-  MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `tbl_book_order`
+--
+ALTER TABLE `tbl_book_order`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `tbl_user_contact`
@@ -229,13 +295,13 @@ ALTER TABLE `tbl_user_contact`
 -- AUTO_INCREMENT for table `tbl_user_data`
 --
 ALTER TABLE `tbl_user_data`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `tbl_user_reviews`
 --
 ALTER TABLE `tbl_user_reviews`
-  MODIFY `review_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `review_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- Constraints for dumped tables
