f96096a1a8764b93366cf36351e5151c571d0e52
ansari7016 <ammaransari7016@gmail.com>
2022-08-18 15:36:39 +0500

    ammar/all products

--- E-Books/views/all_products.php
+++ E-Books/views/all_products.php
@@ -15,63 +15,67 @@ include "../apis/connection.php";
     <title>All Products</title>
 </head>
 
+<style>
+    /* All Products */
+    .products .box-container {
+        max-width: 1200px;
+        margin: 0 auto;
+        display: grid;
+        grid-template-columns: repeat(auto-fit, 30rem);
+        align-items: flex-start;
+        gap: 1.5rem;
+        justify-content: center;
+    }
+
+    .products .box-container .box {
+        border-radius: .5rem;
+        background-color: var(--white);
+        box-shadow: var(--box-shadow);
+        padding: 2rem;
+        text-align: center;
+        border: var(--border);
+        position: relative;
+    }
+
+    .products .box-container .box .image {
+        height: 30rem;
+    }
+
+    .products .box-container .box .name {
+        padding: 1rem 0;
+        font-size: 2rem;
+        color: var(--black);
+    }
+
+    .products .box-container .box .price {
+        font-size: 2.2rem;
+        color: var(--black);
+        padding-top: 1rem;
+    }
+</style>
+
+
 <body>
 
     <?= include "nav.php"; ?>
 
-    <section class="featured" id="bestselling">
+    <section class="products">
 
-        <h1 class="heading"><span>All Products</span></h1>
+        <h1 class="title">latest products</h1>
 
-        <div class="swiper featured-slider">
+        <div class="box-container">
 
-            <div class="swiper-wrapper">
+            <form action="" method="post" class="box">
+                <img class="image" src="image/book-1.png" alt="">
+                <div class="name">A Gentleman in Moscow</div>
+                <div class="price">Rs.1700</div>
+                <!-- <input type="number" min="1" name="product_quantity" value="1" class="qty"> -->
+                <input type="hidden" name="product_name" value="A Gentleman in Moscow">
+                <input type="hidden" name="product_price" value="1700">
+                <input type="hidden" name="product_image" value="A Gentleman in Moscow.png">
+                <input type="submit" value="add to cart" name="add_to_cart" class="btn-n">
+            </form>
 
-                <?php
-
-                $category = mysqli_query($con, "SELECT * FROM `tbl_book_category` WHERE `b_category`='Best Sellers'");
-                $category_row = mysqli_fetch_array($category);
-                $fetch_book = mysqli_query($con, 'SELECT * FROM tbl_book_detail
-                WHERE book_category_1=' . $category_row[0] . ' OR book_category_2=' . $category_row[0] . ' OR book_category_3=' . $category_row[0] . '
-                ');
-                while ($row = mysqli_fetch_array($fetch_book)) {
-                    echo '
-                <div class="swiper-slide box">
-                <div class="icons">
-                <a href="#" class="fas fa-search"></a>
-                <a href="#" class="fas fa-heart"></a>
-                <a data-bs-toggle="modal" data-bs-target="#myModal' . $row[0] . '" style="background: transparent;">
-                <p class="fas fa-eye" data-bs-toggle="tooltip" title="Book Details"></p>
-                </a>
-                </div>
-                <div class="image">
-                <img src="../../dashboard/views/' . $row[17] . '" alt="">
-                </div>
-                <div class="content">
-                <h3>' . $row[1] . '</h3>
-                <div class="price">Rs.' . $row[14] . ' <span>$20.99</span></div>
-                <form action="../apis/cart_apis/add_to_cart.php" method="POST">
-                        <input type="hidden" name="book_name" value="' . $row[1] . '">
-                        <input type="hidden" name="book_price" value="' . $row[14] . '">
-                        <input type="hidden" name="book_image" value="' . $row[17] . '">
-                        <input type="hidden" value="' . $row[0] . '" name="book_id" />';
-                    if (isset($_SESSION["userid"]) != null) {
-                        echo '
-                        <input type="submit" class="btn-n" name="add_to_cart" value="Add To Cart">
-                </form>
-                        ';
-                    } else {
-                        echo '
-                        <a href="#" class="btn-n">Add To Cart</a>';
-                    }
-                    echo '
-                    </div>
-                </div>
-                    ';
-                };
-
-                ?>
-            </div>
         </div>
 
     </section>
